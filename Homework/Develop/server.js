c1ee4d90df23722fc507271c6ed2487ba4e6751a
Demetrio <demetrioalmeida@gmail.com>
2020-06-13 16:24:52 -0400

    Homework

--- Homework/Develop/server.js
+++ Homework/Develop/server.js
@@ -15,6 +15,7 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static("public"));
 
 // === NOtes data ===
 // =================================================
@@ -25,12 +26,23 @@ let notes = [];
 // ==================================================
 // Basic route that sends user first to the AJAX page
 
+
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "public/index.html"));
 });
 
 app.get("/notes", (req, res) => {
     res.sendFile(path.join(__dirname, "public/notes.html"));
+});
+
+app.get("/api/notes", (req, res) => {
+    return res.json(notes);
+});
+
+// Creates a new note - takes in JSON input
+
+app.post("api/notes", function(req, res) {
+
 });
 
 // Start the server to begin listening
